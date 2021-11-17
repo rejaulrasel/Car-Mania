65a6374317ec78c32f0ee9c9e8d2eac9eaeb4677
rejaulrasel <rejaulhasanrasel28@gmail.com>
2021-11-17 22:38:17 +0600

    added firebase setiup

---
+++ src/Pages/Login/Firebase/Firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAJzGvGkmb1w1cnm-9majAtQ2rRMe6w6RQ",
+    authDomain: "car-mania-f1b76.firebaseapp.com",
+    projectId: "car-mania-f1b76",
+    storageBucket: "car-mania-f1b76.appspot.com",
+    messagingSenderId: "583779582222",
+    appId: "1:583779582222:web:32cebb8a0a62e510ac175f"
+  };
+  export default firebaseConfig;

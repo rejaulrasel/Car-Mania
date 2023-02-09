d593fa529ad179e2b02a664a45f71dd19c41a657
rejaulrasel <rejaulhasanrasel28@gmail.com>
2023-02-09 10:29:13 +0600

    edit gitignore file

---
+++ build/imageLink.js
@@ -0,0 +1,16 @@
+const links = [
+    https://i.ibb.co/8zrtQp5/9-V-BATTERY.jpg
+    https://i.ibb.co/qmDNySK/Arduino-pro-micro.jpg
+    https://i.ibb.co/zxMy22d/Arduino-UNO-R3.png
+    https://i.ibb.co/3T56fQY/arduino-uno-large-comp.jpg
+    https://i.ibb.co/6Y3pTrg/arduio-nano.png
+    https://i.ibb.co/jw9kLs5/blue.jpg
+    https://i.ibb.co/P5wmWhr/breadboard.png
+    https://i.ibb.co/HXZx6CP/ir-sensor.jpg
+    https://i.ibb.co/kQBB9xt/jumper.jpg
+    https://i.ibb.co/5nk17Dx/nizzah-khusnunnisa-1-Zlf-Gq1-PGJc-unsplash.jpg
+    https://i.ibb.co/098mVCm/samsung-memory-l7b-Jq-VHx-YA-unsplash.jpg
+    https://i.ibb.co/tHNqrmN/depositphotos-198457778-stock-photo-integrated-circuit-or-micro-chip.webp
+    https://i.ibb.co/r4VkY94/ic-555-timer-500x500.png
+    https://i.ibb.co/WybBRCq/download.jpg
+];
